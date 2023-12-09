da4635d49ee4f8f4988bd5244a69cfcc9bf417c0
ft191298yna <mykola.yevsihneiev@monobank.ua>
2023-12-09 15:09:48 +0200

    added main window

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,12 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {SharedModule} from "./shared/shared.module";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {LoaderInterceptor} from "./shared/interceptors/loader.interceptor";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
